1ed2f99f869d941709dd1ea91c27f3bfe8919688
Samantha Bast <basts@roverkids.org>
2025-05-21 13:39:26 -0400

    Finished 4.4

--- GameplayMechanics/GameplayMechanics/Assets/Scripts/SpawnManager.cs
+++ GameplayMechanics/GameplayMechanics/Assets/Scripts/SpawnManager.cs
@@ -6,22 +6,44 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
-         Vector3 GenerateSpawnPosition ()
-        {
-            float spawnPosX = Random.Range(-spawnRange, spawnRange);
-            float spawnPosY = Random.Range(-spawnRange, spawnRange);
-            Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosX);
-            return randomPos;
-        }
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;  
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    
+   private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosX);
+        return randomPos;
+    }
+
 }
